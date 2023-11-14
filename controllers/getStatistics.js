8a476158e28402f56009700fb307623021936a53
sravankumar-samala <sravankumarsamala20@gmail.com>
2023-11-14 22:16:09 +0530

    Updated Statistics API

--- controllers/getStatistics.js
+++ controllers/getStatistics.js
@@ -1,17 +1,20 @@
 import { db } from '../app.js'
 
 const getStatistics = async (req, res) => {
-    // const {month} = req.query
-    const month = 7
-    const getTransactionsQuery = `SELECT 
+  let { month } = req.query
+  month = +month || 3
+  const getStatisticsQuery = `SELECT 
                                     SUM(CASE WHEN sold = 1 THEN price ELSE 0 END) AS total_sale_amount,
                                     COUNT(CASE WHEN sold = 1 THEN 1 END) AS total_sold_items,
                                     COUNT(CASE WHEN sold = 0 THEN 1 END) AS total_unsold_items
                                   FROM product_transactions
                                   WHERE CAST(strftime('%m', dateOfSale) AS INTEGER) = ${month};`
-
-    const transactions = await db.all(getTransactionsQuery)
-    res.send(transactions)
+  try {
+    const statistics = await db.all(getStatisticsQuery)
+    res.send(statistics)
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 export default getStatistics
